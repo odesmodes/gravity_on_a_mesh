b4bbef60db54b14ae6af75d79051b0869cd5f2f9
VM2708 <vm2708@nyu.edu>
2024-10-07 17:07:39 -0400

    Create InitializePoints.py

---
+++ InitializePoints.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 16:42:15 2024
+
+@author: vedhasyamuvva
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def initializeGaussianPoints(center, a, ba, ca, N = 32**3):
+    mean = np.array(center)
+    cov = np.diag(np.array([a**2, (a*ba)**2, (a*ca)**2]))
+    
+    return np.random.multivariate_normal(mean, cov, N)
+
+def plotInitialPoints(arr):
+    
+    ax = plt.figure().add_subplot(projection='3d')
+    arr = arr.T
+    ax.scatter(arr[0], arr[1], arr[2], '.', label='initial points')
+    
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
+    ax.set_zlabel('Z-axis')
+    plt.title('3D Scatter Plot of Gaussian Distributed Points')
+    
+    
+    plt.show()

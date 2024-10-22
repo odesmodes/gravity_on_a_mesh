8d900e52f179f84bc5a1ea43bf3b2b3f9210c6a8
VM2708 <vm2708@nyu.edu>
2024-10-22 19:08:30 -0400

    More progress on Problem 2, incorrect density vals

--- InitializePoints.py
+++ InitializePoints.py
@@ -11,13 +11,45 @@ Created on Mon Oct  7 16:42:15 2024
 import numpy as np
 import matplotlib.pyplot as plt
 
+""" Generates N random points according to a gaussian distribution set to the chosen parameters
+ 
+Inputs:
+ ------
+ center : NumPy array
+     The center point of the gaussian distribution
 
+ a : NumPy value
+     The length of the semimajor axis
+
+ ba : NumPy value
+     The axis ratio of the second axis to the semimajor axis
+
+ ca : NumPy value
+     The axis ratio of the third axis to the semimajor axis
+ 
+ N : NumPy integer
+     The number of points to be generated, the default is 32x32x32 points
+
+ Returns:
+ -------
+ points: Numpy array
+    N random points generated with the chosen parameters
+
+"""
 def initializeGaussianPoints(center, a, ba, ca, N = 32**3):
     mean = np.array(center)
     cov = np.diag(np.array([a**2, (a*ba)**2, (a*ca)**2]))
     
-    return np.random.multivariate_normal(mean, cov, N)
+    points = np.random.multivariate_normal(mean, cov, N)
+    return points
 
+""" Plots the points generated with initialize Gaussian Points
+ 
+Inputs:
+ ------
+ arr : NumPy array
+     array of N 3D points
+"""
 def plotInitialPoints(arr):
     
     ax = plt.figure().add_subplot(projection='3d')
@@ -31,3 +63,92 @@ def plotInitialPoints(arr):
     
     
     plt.show()
+    
+""" Calculates the Density field for a single point in the array
+ 
+Inputs:
+ ------
+ point : NumPy array
+     The point at which the density will be calculated
+
+ a : NumPy value
+     The length of the semimajor axis
+
+ ba : NumPy value
+     The axis ratio of the second axis to the semimajor axis
+
+ ca : NumPy value
+     The axis ratio of the third axis to the semimajor axis
+ 
+ arr : NumPy array
+     the array of points previously generated randomly
+
+ Returns:
+ -------
+ dens: Numpy value
+    the density of of the cell around "point"
+
+"""    
+def calculateDensityOfPoint(point, a, ba, ca, arr):
+    #Theoretically these values should be divided by 32
+    La = a
+    Lb = ba*a
+    Lc = ca*a
+    #print(La,Lb,Lc)
+    distance = np.abs(arr-point)
+    #print(arr)
+    #print(distance)
+    dens = 0
+    
+    for i in range (np.shape(distance)[0]):
+        if (distance[i][0]<= (La/2) and distance[i][1] <= (Lb/2) and distance[i][2] <= (Lc/2)):
+            dens += ((La - distance[i][0])*(Lb - distance[i][1])*(Lc - distance[i][2]))
+    return dens
+
+""" Calculates the Density field for every point and plot some results
+ 
+Inputs:
+ ------
+ center : NumPy array
+     The point at which the points have been centered around
+
+ a : NumPy value
+     The length of the semimajor axis
+
+ ba : NumPy value
+     The axis ratio of the second axis to the semimajor axis
+
+ ca : NumPy value
+     The axis ratio of the third axis to the semimajor axis
+ 
+ arr : NumPy array
+     the array of points previously generated randomly
+
+ Returns:
+ -------
+ densityField: Numpy array
+    the density of of the each cell arranged in a somewhat convoluted fashion that needs to be fixed
+
+"""  
+def PlotDensityField(center, a, ba, ca, arr):
+    #Create a a NxNxN Grid of points to calculate the density for 
+    x = np.arange(32) * a + center[0]
+    y = np.arange(32) * ba * a + center[1]
+    z = np.arange(32) * ca * a + center[2]
+
+    # Create the meshgrid
+    X, Y, Z = np.meshgrid(x, y, z)
+
+    # Stack X, Y, Z along a new axis to create a grid of grids
+    #points = np.stack([X, Y, Z], axis=-1)
+    grid_points = np.vstack([X.ravel(), Y.ravel(), Z.ravel()]).T
+    
+    #Get values for each point
+    densityField = [calculateDensityOfPoint(point, a, ba, ca, arr) for point in grid_points]
+    
+    #Plot along each of the axes
+    plt.plot(grid_points[:,16,16],densityField[:,16,16], '.')
+    plt.show()
+    
+    
+    return densityField
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 16:40:32 2024
+
+@author: vedhasyamuvva
+"""
+
 import InitializePoints
+import numpy as np
 
 center = [0,0,0]
 a = 1
@@ -10,3 +19,8 @@ N = 32**3
 arr = InitializePoints.initializeGaussianPoints(center, a, ba, ca)
 
 InitializePoints.plotInitialPoints(arr)
+
+testpoint = np.array([0,0,0])
+print(InitializePoints.calculateDensityOfPoint(testpoint,a,ba,ca, arr))
+
+#InitializePoints.PlotDensityField(center, a, ba, ca, arr)

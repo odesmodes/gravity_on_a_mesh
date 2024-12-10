db3126ece4f25faa833604559cdec536578b3714
jferrante25 <jf5156@nyu.edu>
2024-12-10 16:10:19 -0500

    updated Poisson eq files

--- PoissonEquation.py
+++ PoissonEquation.py
@@ -8,17 +8,26 @@ def PoissonsEq(density):
  '''Takes density as an input, solves Poissons eq using Fourier Transformed density, inverse Fourier Transforms result to return solution for potential'''
 #Take fft of density 
  density_dft = sp.fft.fftn(density)
- potential_dft = np.empty((32,32,32),dtype = 'complex_')
- for x in range(1,len(density_dft)):
-  for y in range(1,len(density_dft[x])):
-   for z in range(1,len(density_dft[x][y])):
-    potential_dft[x][y][z]=1/(2*(np.cos(2*x*np.pi/len(density_dft))+np.cos(2*y*np.pi/len(density_dft[x]))+np.cos(2*z*np.pi/len(density_dft[x][y])) -3))*4*np.pi*density_dft[x][y][z]
-  print('potential dft: '+str(potential_dft))
- potential_ift = sp.fft.ifftn(potential_dft)
- potential=potential_ift.real
- print('potential: '+str(potential))
 
- return potential
+#Define array to hold F-transformed potential  
+ potential_dft = np.empty((32,32,32),dtype = 'complex_')
+ 
+#loop over density FFT and multiply by expression discrete transform  
+ for x in range(0,len(density_dft)):
+  for y in range(0,len(density_dft[x])):
+   for z in range(0,len(density_dft[x][y])):
+    if 2*(np.cos(2*x*np.pi/len(density_dft))+np.cos(2*y*np.pi/len(density_dft[x]))+np.cos(2*z*np.pi/len(density_dft[x][y])) -3)==0:
+    #if expression is 0, set potential dft to 0
+     potential_dft[x][y][z]=0
+    else: 
+     potential_dft[x][y][z]=1/(2*(np.cos(2*x*np.pi/len(density_dft))+np.cos(2*y*np.pi/len(density_dft[x]))+np.cos(2*z*np.pi/len(density_dft[x][y])) -3))*4*np.pi*density_dft[x][y][z]
+    
+#take inverse FT of potental FT
+ potential_ift = sp.fft.ifftn(potential_dft)
+ 
+#take real part of potential and return 
+ potential=potential_ift.real
+  return potential
 
  
 def PlotPotential2D(potential, x,y,z, axis, value):
@@ -51,36 +60,56 @@ def PlotPotential2D(potential, x,y,z, axis, value):
 def IsolatedMass(density):
  """Solves Poisson's equation for an isolated mass distribution using a convolution of the Fourier transforms of the mass m"""
 #create extended mesh to isolate mass 
- iso_den= np.resize(density,(64,64,64))
-#create Green's function with restricted range
+ iso_den=np.zeros((64,64,64))
+
+#create array for Green's function with restricted range
  G=np.zeros((64,64,64))
-#Greens' function on 32*3 grid 
- 
- G32=np.zeros((32,32,32))
-#loop over points except origin and set to 1/|r| 
- for x in list(range(1,16)) +list(range(17,32)):
-  for y in list(range(1,16)) +list(range(17,32)):
-   for z in list(range(1,16)) +list(range(17,32)):
-    G[x][y][z]= 1/np.sqrt((16-x)**2 + (16-y)**2+(16-z)**2)   
-#set origin to 1    
-    G32[x][y][z]=G[x][y][z]
- G[16][16][16]=1
- G32[16][16][16]=1
- print(G32[16][16][16])
- print(G32[15][15][15])
- print(G32)
+
+#loop over points and set to 1/|r| where r is the distance from each outermost vertex of the extended cubic mesh, within each of the 8 smaller cubic regions containing these vertices. The active region in which the mass distribution is contained is x,y,z <32
+ for x in range(0,64):
+  for y in range(0,64):
+   for z in range(0,64):
+    if x<32:
+     xval=x
+    else:
+     xval=(64-x) 
+    if y<32:
+     yval=y
+    else:
+     yval=(64-y)   
+    if z<32:
+     zval =z
+    else:
+     zval=(64-z)
+    r2=(xval**2 +yval**2+zval**2)
+    if r2==0:
+     G[x][y][z]=1
+    else: 
+     G[x][y][z]=1/np.sqrt(r2)
+#set the iso_den to density value with in active region     
+    if x<32 and y < 32 and z < 32: 
+     iso_den[x][y][z]=density[x][y][z]   
+
+
 #FT Green's function
- G_dft= sp.fft.fftn(G)
- 
-#
+ G_ft= sp.fft.fftn(G)
 #FT density
  density_ft=sp.fft.fftn(iso_den) 
- potential_dft = G_dft*density_ft
- potential_ift = sp.fft.ifftn(potential_dft)
- potential=potential_ift.real
- #
- return G32
 
+#Define Potential FT array 
+ potential_ft=np.zeros((64,64,64),dtype = 'complex_')
  
+#set potential ft to G_ft *density_ft 
+ for x in range(0,64):
+  for y in range(0,64):
+   for z in range(0,64):
+    potential_ft[x][y][z] = G_ft[x][y][z]*density_ft[x][y][z]
+
+#invert potential FT and take real part  
+ potential_ift = sp.fft.ifftn(potential_ft)
+ potential=potential_ift.real
+
+ return potential
+
 
 
--- PoissonTest.py
+++ PoissonTest.py
@@ -11,17 +11,41 @@ N = 32**3
 grid_size = 32
 
 
-#central point Poisson test 
-testpoint = np.array( [0, 0, 0])
-arr = np.array([testpoint])
 
-densityField, x,y,z = InitializePoints.CreateDensityField(center, a, ba, ca, arr, grid_size)
+a = .2
+ba = 1
+ca = 1
+N = 32**3
+    
+particles = InitializePoints.initializeGaussianPoints(center, a, ba, ca, N)
+
+#central point Poisson test 
+testpoint = np.array([0,0,0])
+
+
+arr = particles #
+#arr=np.array([testpoint])
+
+densityField, x,y,z = InitializePoints.CreateDensityField(center,particles, grid_size)
 InitializePoints.plotInitialPoints(arr)
-densityField, x,y,z = InitializePoints.CreateDensityField(center, a, ba, ca, arr, grid_size)
-testpotential=PoissonEquation.PoissonsEq(densityField)   
-PoissonEquation.PlotPotential2D(testpotential, x,y,z,'x',int(grid_size/2))
-PoissonEquation.PlotPotential2D(testpotential, x,y,z,'y', int(grid_size/2))
-PoissonEquation.PlotPotential2D(testpotential, x,y,z,'z', int(grid_size/2))
+
+#densityField, x,y,z = InitializePoints.CreateDensityField(center, arr, grid_size)
+#testpotential=PoissonEquation.PoissonsEq(densityField)   
+
+ 
+testpotential=PoissonEquation.IsolatedMass(densityField)   
+
+#this creates the new axes for the extended mesh
+x2 = np.linspace(-.5, 1.5, 64, endpoint=True) + center[0]
+y2 = np.linspace(-.5, 1.5, 64, endpoint=True) + center[1]
+z2 = np.linspace(-.5, 1.5, 64, endpoint=True) + center[2]
+
+#This plots the potential using the density plotter function, so the axis label is wrong
+InitializePoints.PlotTestFields(testpotential, x2, y2, z2, 32)
+
+
+
+
 
 
 ''' 

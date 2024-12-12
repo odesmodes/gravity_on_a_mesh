56bdab254217aea93bf74018c0c8896f4d34b4cd
VM2708 <vm2708@nyu.edu>
2024-12-11 21:17:53 -0500

    Added spherical tests around a single point

--- Pytest.py
+++ Pytest.py
@@ -12,6 +12,7 @@ import pytest
 
 #UNIT TESTS
 def test_pointAtCenterOfCell():
+    
     center = [0,0,0]
     grid_size = 32
     
@@ -19,13 +20,27 @@ def test_pointAtCenterOfCell():
     y = np.linspace(-0.5, 0.5, grid_size, endpoint=True) + center[1]
     z = np.linspace(-0.5, 0.5, grid_size, endpoint=True) + center[2]
     
+    #Place one particle at exactly hte middle point of the grid
     particles = np.array([[x[16],y[16],z[16]]])
     
+    #Test the density field is nonzero only at that point
     densityField,_,_,_ = InitializePoints.CreateDensityField(center, particles, grid_size)
-    assert (densityField[16][16][16] == 1)
+    assert (densityField[16][16][16] != 0)
     print(densityField[16][16][16])
     assert (densityField[15][15][16] == 0)
     assert (densityField[15][16][15] == 0)
     assert (densityField[16][15][15] == 0)
 
+    #Test that phi is symmetrical around that one point
+    phi = PoissonEquation.IsolatedMass(densityField)
+    assert (phi[15][16][16] == phi[16][15][16] and 
+            phi[16][16][15] == phi[16][15][16])
+    
+    assert (phi[17][16][16] == phi[16][17][16] and 
+            phi[16][16][17] == phi[16][17][16])
+    
+    assert (phi[15][16][16] == phi[16][17][16])
+    
+
+                                     
 test_pointAtCenterOfCell()

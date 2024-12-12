31536b6d5d82c0d7e549c30f7ab2f4ea71504fd5
VM2708 <vm2708@nyu.edu>
2024-12-12 15:17:51 -0500

    extra time step shenanigans

--- CreateAnimation.py
+++ CreateAnimation.py
@@ -21,23 +21,27 @@ ba = 1
 ca = 1
 N = 32**3
 grid_size = 32
-dt = 10
+dt = .1
 
-particles = InitializePoints.initializeGaussianPoints(center, a, ba, ca, 15)
+numStep = 1
+particles = InitializePoints.initializeGaussianPoints(center, a, ba, ca, N)
 densityField, _,_,_ = InitializePoints.CreateDensityField(center, particles, grid_size)
 
 
 velocities = np.zeros_like(particles) 
+#velocities = IntegrationMethods.NetAngularMomentum(particles)
 
 phi = PoissonEquation.IsolatedMass(densityField)
-velocities = dt/2 * IntegrationMethods.F(particles, IntegrationMethods.gradient(phi))
+velocities = velocities + dt/2 * IntegrationMethods.F(particles, IntegrationMethods.gradient(phi))
 
 
 # Update for each frame, particles and velocities get updated
 def update(frame):
-    global particles, velocities
+    global particles, velocities, numStep, dt
     particles, velocities = IntegrationMethods.xnext(particles, velocities, dt)
     scat._offsets3d = (particles[:, 0], particles[:, 1], particles[:, 2])
+    numStep += 1
+    if (numStep % 10 == 0): dt = dt*0.9
     return scat,
 
 # Initialize and plot initialized particles and create bounds

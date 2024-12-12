06c6da705cdc778a337ab89c214fc8ed6ff6f21d
VM2708 <vm2708@nyu.edu>
2024-12-11 20:03:28 -0500

    added comments and did some reorganization

--- CreateAnimation.py
+++ CreateAnimation.py
@@ -11,86 +11,68 @@ import matplotlib.animation as animation
 import InitializePoints
 import TestPhi
 import PoissonEquation
+import IntegrationMethods
 
 
-# Creating a function to calculate the gradient of the potential at position x with a step size for finite difference
-# This is because in order to find the velocities we first need to find the acceleration, which we can get from F = ma = -grad(phi)
-def gradient(phi, spacing = 1/32):
-    grad = np.gradient(phi, spacing)
-    return np.array([grad[0],grad[1],grad[2]])
+"""
+______________________________________________________________________________
+Run this file to create an animation Exploring the Gravitational Collapse
+______________________________________________________________________________
 
-# Creating a function to calculate the force on each particle at position x
-# Force is just negative the gradient of the potential
-
-def F(particles, gradArr, spacing=1/32):
-    # Calculate the grid indices for each particle's position
-    indices = ((particles + 0.5) / spacing).astype(int)
+Adjustable Paramters:
+    a: Numpy Value
+        semi major axis
+        
+    ba: Numpy Value 
+        first semi minor to semi major axis ratio
     
-    # Initialize forces array
-    forces = np.zeros_like(particles)
+    ca: Numpy Value
+        second semi minor to semi major axis ratio
+        
+    N: Number Value
+        number of particles in the simulation
     
-    # Check if indices are within valid bounds
-    in_bounds = (indices[:, 0] >= 0) & (indices[:, 0] < gradArr.shape[1]) & \
-                (indices[:, 1] >= 0) & (indices[:, 1] < gradArr.shape[2]) & \
-                (indices[:, 2] >= 0) & (indices[:, 2] < gradArr.shape[3])
+    dt: Numpy Value
+        time step
     
-    # Apply forces only to particles within bounds
-    valid_indices = indices[in_bounds]
-    forces[in_bounds] = gradArr[:, valid_indices[:, 0], valid_indices[:, 1], valid_indices[:, 2]].T
-     
-    return -forces
+"""
 
-# Parameters
-center = [0,0,0]
-a = .2
-ba = 1
-ca = 1
-N = 32**3
-grid_size = 32
-dt = 10
+center = [0,0,0] #Define center of the diagram (usually set to [0,0,0])
+a = .2   #Value of semimajor axis
+ba = 1   #Value of first semi minor to semi major axis ratio
+ca = 1   #Value of second semi minor to semi major axis ratio
+N = 15   #Number of Particles in the simulation
+dt = 10  #Time Step
 
-particles = InitializePoints.initializeGaussianPoints(center, a, ba, ca, 15)
-densityField, _,_,_ = InitializePoints.CreateDensityField(center, particles, grid_size)
-
-# Assume velocities = v0 are at rest
-# Then  v1/2 = dt/2 * F(x) 
-
-# Define an array for velocities that is the same size as the particles array
-#print("particles: ", np.shape(particles))
+#Initialize the particles and the velocities
+particles = InitializePoints.initializeGaussianPoints(center, a, ba, ca, N)
 velocities = np.zeros_like(particles) 
 
+#Calculate the first velocity half step
+densityField, _,_,_ = InitializePoints.CreateDensityField(center, particles, grid_size=32)
 phi = PoissonEquation.IsolatedMass(densityField)
-velocities = dt/2 * F(particles, gradient(phi))
+velocities = dt/2 * IntegrationMethods.F(particles, IntegrationMethods.gradient(phi))
 
-# THIS IS from the Verlet Method
-def xnext(x, v, dt=dt): 
-    #RECALCULATE PHI
-    densityField, _,_,_ = InitializePoints.CreateDensityField(center, x, grid_size)
-    phi = PoissonEquation.IsolatedMass(densityField)
-    gradArr = gradient(phi)
-    
-    # Calculate gradArr with new distribution
 
-    # VERLET METHOD FROM WIKIPEDIA
-    F_old = F(x, gradArr)
-    x_new = x + v * dt + 0.5 * F_old * dt**2
-    gradArr_new = gradient(phi)
-    F_new = F(x_new, gradArr_new)
-    v_new = v + 0.5 * (F_old + F_new) * dt
 
-    #v_new = v + F(x, gradArr) * dt/2
-    #print("x: ", np.shape(x), np.shape(v_new))
-    #x_new = x + v_new * dt
-    return x_new, v_new
+""" Update the next animation frame
+Inputs:
+ ------
+ frame: value
+     the number of times the update function needs to run
 
-# Update for each frame, particles and velocities get updated
+ Returns:
+ -------
+ scat,: Matplotlib.pyplot.plot.figure
+     the plot showing particle position
+"""
 def update(frame):
     global particles, velocities
-    particles, velocities = xnext(particles, velocities, dt)
+    particles, velocities = IntegrationMethods.xnext(particles, velocities, dt)
     scat._offsets3d = (particles[:, 0], particles[:, 1], particles[:, 2])
     return scat,
 
-# Initialize and plot initialized particles and create bounds
+#Initialize the new plot
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 scat = ax.scatter(particles[:, 0], particles[:, 1], particles[:, 2], label='Particles')
@@ -99,9 +81,8 @@ ax.set_ylim(-0.6, 0.6)
 ax.set_zlim(-0.6, 0.6)
 
 ax.set_title("Exploring The Physics Of Gravitational Collapse")
+
 # Animate
 ani = animation.FuncAnimation(fig, update, frames=5, interval=100, blit=False)
 ani.save('grav_collapse.mp4', fps=20, extra_args=['-vcodec', 'libx264'])
 plt.show()
-
-
